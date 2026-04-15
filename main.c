770a7b0e69706af14700d821ce3d4a3ec3c318bc
David Canizares <scaniizares@gmail.com>
2026-04-14 21:05:27 -0500

    Portada inicial

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 /*
+            UNIVERSIDAD DE LAS AMÉRICAS
+    FACULTAD DE INGENIERÍA Y CIENCIAS APLICADAS
+            INGENIERÍA DE SOFTWARE
+                PROGRAMACIÓN I
+Autores: 
+    -David Cañizares
+    -Sebastián Orbe
+    -Jean Pierre Rosas
+    -Derek Yépez
 
+============== SISTEMA DE CALIFICACIONES =============
 */
+
 #include <stdio.h>
+#include <funciones.h>
+
 int main(){
     
+    return 0;
 }
